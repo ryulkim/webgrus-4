ecd9d0f1562746457f93c483e73767afb4afd646
ryula <hobby519@naver.com>
2021-05-12 00:42:23 +0900

    data types

---
+++ VanillaJS/Data types.js
@@ -0,0 +1,15 @@
+//String
+const what = "Ryula";
+console.log(what);
+
+//boolean
+const wat = false; //소문자로 씀, 0or 1, false or true
+console.log(wat);
+
+//float
+const wt = 55.1;
+console.log(wt);
+
+//number
+const ww = 666;
+console.log(ww);
