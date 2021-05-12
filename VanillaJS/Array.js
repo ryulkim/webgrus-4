3348e3c59757790c1dedb62645fc547da6bdc251
ryula <hobby519@naver.com>
2021-05-12 11:24:21 +0900

    Array

---
+++ VanillaJS/Array.js
@@ -0,0 +1,16 @@
+//camel case, 스페이스 대신 대문자로 씀 구불구불 높낮이 다름
+const something = "Something";
+const daysOfWeek = [
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+  "Sun",
+  true,
+  54,
+  0.00001,
+  something,
+];
+console.log(daysOfWeek);
