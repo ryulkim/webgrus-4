835d18560acacca3d94158ebe55e3da63ed6bbdf
ryula <hobby519@naver.com>
2021-05-12 14:40:59 +0900

    Function

---
+++ VanillaJS/function.js
@@ -0,0 +1,9 @@
+//Function: 원하는 만큼 쓸 수 있는 코드
+//인자(argument)=파라미터
+
+function sayHello(name, num) {
+  console.log("Hello!", name + "!!!", "your number is", num);
+}
+
+sayHello("Ryula", 15);
+console.log("hi");
