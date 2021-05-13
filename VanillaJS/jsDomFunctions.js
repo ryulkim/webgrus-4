82259d21918e0420b4670e8e01fb80b473cb88fe
ryula <hobby519@naver.com>
2021-05-13 11:39:04 +0900

    JSDomFunctions

---
+++ VanillaJS/jsDomFunctions.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("title");
+console.log(title);
+console.log(document);
+
+//document를 js 단독으로 실행할 경우 에러가 남. html을 실행하고 console로 가서 봐야함
+
+console.error("FUck");
+
+title.innerHTML = "HI! everyon!";
