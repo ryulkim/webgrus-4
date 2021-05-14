7573f86047c49b6d4d8a585511f814b650a108d2
ryula <hobby519@naver.com>
2021-05-15 02:26:32 +0900

    js로 title className 지정+천천히 변화 주기

---
+++ VanillaJS/if,else_practic_two.js
@@ -0,0 +1,20 @@
+const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
+
+const BASE_COLOR = "rgb(0, 247, 255)";
+const OTHER_COLOR = "#f9ca24";
+
+function handleClick() {
+  const currentClass = title.className;
+  if (currentClass !== CLICKED_CLASS) {
+    title.className = CLICKED_CLASS;
+  } else {
+    title.className = "";
+  }
+}
+
+function init() {
+  window.addEventListener("click", handleClick);
+}
+
+init();
