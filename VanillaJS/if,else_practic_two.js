1586e3e47ab0c00cb0d288a08c7af72d0cfed26b
ryula <hobby519@naver.com>
2021-05-15 03:44:16 +0900

    toggle 사용

--- VanillaJS/if,else_practic_two.js
+++ VanillaJS/if,else_practic_two.js
@@ -5,7 +5,7 @@ const BASE_COLOR = "rgb(0, 247, 255)";
 const OTHER_COLOR = "#f9ca24";
 
 function handleClick() {
-  title.classList.toggle(CLICKED_CLASS); //toggle로 아까 했던 contain 작업 반복 super simple~!
+  title.classList.toggle(CLICKED_CLASS); //toggle로 아까 했던 contain 작업 반복 super simple~!, toggle이 CLICKED_CLASS가 있는지 체크하고 add하거나 remove한다.
 }
 
 function init() {
