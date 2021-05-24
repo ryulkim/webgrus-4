a3aff1fee9540a9a54ec96074895c77eca34dd73
ryula <hobby519@naver.com>
2021-05-24 12:22:16 +0900

    위치: 위도, 경도

---
+++ VanillaJS/MakeApp/weather.js
@@ -0,0 +1,37 @@
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoErro() {
+  console.log("Can't access goe location");
+}
+
+function askforCords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoErro);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords == null) {
+    askforCords();
+  } else {
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
