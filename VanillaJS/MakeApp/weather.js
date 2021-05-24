6c2f5bab547a1e87d7d706378fe7718e910c76d3
ryula <hobby519@naver.com>
2021-05-24 13:06:13 +0900

    api 사용

--- VanillaJS/MakeApp/weather.js
+++ VanillaJS/MakeApp/weather.js
@@ -1,4 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "6014809f85cd2245c0ebbc9f78846be6"; //손쉽게 데이터를 가져올 수 있는 수단
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature}, ${place}`;
+      console.log(json);
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +30,7 @@ function handleGeoSucces(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoErro() {
@@ -24,9 +43,12 @@ function askforCords() {
 
 function loadCoords() {
   const loadedCoords = localStorage.getItem(COORDS);
-  if (loadedCoords == null) {
+  if (loadedCoords === null) {
     askforCords();
   } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+    console.log(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
