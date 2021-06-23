716b6fad1b7fb597476621d41277ccfad8251732
ryula <hobby519@naver.com>
2021-06-23 16:29:35 +0900

    변수

--- UpdateVanillaJS/VanillaJs/app.js
+++ UpdateVanillaJS/VanillaJs/app.js
@@ -1,1 +1,21 @@
-alert("hi");
+console.log("asdfasdf");
+console.log(123123);
+
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+// 변수
+//const: 바뀌지 않는 값
+//JS에서는 변수 이름의 공백을 대분자로 바꿈 ex) myName
+//파이썬 방식: 공백을 _로 함, ex) my_name
+const a = 7;
+const b = 2;
+const myName = "ryula";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+console.log("Hello,", myName);
+console.log("Hello, " + myName);
