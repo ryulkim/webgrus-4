7305095349142e21f0e76d3f85382152e33f43e6
ryula <hobby519@naver.com>
2021-06-24 16:44:59 +0900

    array

--- UpdateVanillaJS/VanillaJs/app.js
+++ UpdateVanillaJS/VanillaJs/app.js
@@ -24,8 +24,19 @@ myName = "RyulA";
 console.log("Hello, " + myName);
 
 //boolean
-//0: true, 1: false, null: 아무것도 없음
+//0: true, 1: false, null: 아무것도 없음, undefined: 정의 되지 않음
+//null은 값이 없다는걸 확실하게 표현, 의도적으로 표현
+//undefined는 variable이 존재하는데 값이 할당이 안된것
 const amIFat = false;
 let somthing;
 console.log(amIFat);
 console.log(somthing);
+
+//array
+const nonsense = [1, 2, "hello", false, true];
+console.log(nonsense);
+//Get Item from Array
+console.log(nonsense[2]);
+//Add one more day to the array
+nonsense.push("ddddd");
+console.log(nonsense);
