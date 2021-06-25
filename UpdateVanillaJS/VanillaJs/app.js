5b13a9ada27642f2859534c7644bf2404003e4f0
ryula <hobby519@naver.com>
2021-06-25 13:53:30 +0900

    recap

--- UpdateVanillaJS/VanillaJs/app.js
+++ UpdateVanillaJS/VanillaJs/app.js
@@ -81,3 +81,24 @@ const Player = {
 
 console.log(Player.name);
 Player.sayHello("Ryula");
+
+//HW
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(1, 2);
+calculator.minus(1, 2);
+calculator.divide(1, 2);
+calculator.power(1, 2);
