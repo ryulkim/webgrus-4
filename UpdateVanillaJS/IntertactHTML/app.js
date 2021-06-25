a3a3148ed1a5f099a3dda19268a68500ea66b760
ryula <hobby519@naver.com>
2021-06-25 16:48:44 +0900

    className

--- UpdateVanillaJS/IntertactHTML/app.js
+++ UpdateVanillaJS/IntertactHTML/app.js
@@ -1,5 +1,8 @@
 //document
 document.title = "Hello! From Js!";
 const title = document.getElementById("title");
-console.dir(title);
+//console.dir(title);
 title.innerText = "1234";
+const className = document.getElementsByClassName("hello");
+console.dir(className);
+className[0].innerText = "1231231";
