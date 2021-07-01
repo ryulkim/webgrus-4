0e00cf4e59f7615b34521255c390568f137608df
ryula <hobby519@naver.com>
2021-07-02 02:09:58 +0900

    classList 사용

--- UpdateVanillaJS/PartTwo/app.js
+++ UpdateVanillaJS/PartTwo/app.js
@@ -1,5 +1,5 @@
 const h1 = document.querySelector("div.hello h1");
-console.log(h1);
+console.log(h1); /*
 function handleTitleClick() {
   const clickedClass = "active";
   if (h1.className === clickedClass) {
@@ -7,6 +7,16 @@ function handleTitleClick() {
   } else {
     h1.className = clickedClass;
   }
-}
+}//class name이 있을 때, 오류 발생, 기존의 class name이 사리지고 완전 변경됨
+*/
 
+//classList.contains: html element의 class에 포함되어 있는지 말해줌
+function handleTitleClick() {
+  const clickedClass = "active";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
+}
 h1.addEventListener("click", handleTitleClick);
