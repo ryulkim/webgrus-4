4d9fa7474bcd97bb1e506cb788a355f40c4ccfae
ryula <hobby519@naver.com>
2021-07-02 02:13:25 +0900

    toggle 사용

--- UpdateVanillaJS/PartTwo/app.js
+++ UpdateVanillaJS/PartTwo/app.js
@@ -11,12 +11,16 @@ function handleTitleClick() {
 */
 
 //classList.contains: html element의 class에 포함되어 있는지 말해줌
-function handleTitleClick() {
+/*function handleTitleClick() {
   const clickedClass = "active";
   if (h1.classList.contains(clickedClass)) {
     h1.classList.remove(clickedClass);
   } else {
     h1.classList.add(clickedClass);
   }
+}*/
+//toggle: class name이 존재하는지 확인, class name이 존재한다면 삭제, 존재 안하면 추가
+function handleTitleClick() {
+  h1.classList.toggle("active");
 }
 h1.addEventListener("click", handleTitleClick);
