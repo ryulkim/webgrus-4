dc3140a511093508482232186236875b913a9c02
ryula <hobby519@naver.com>
2021-07-05 01:41:15 +0900

    map recap

--- ReactJS/movie_app/src/App.js
+++ ReactJS/movie_app/src/App.js
@@ -12,38 +12,47 @@ function Food({ fav, picture }) {
   return (
     <div>
       <h3>I like {fav}</h3>
-      <img src={picture} />
+      <img src={picture} alt={fav} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
   },
 ];
+
+/* function renderFood(dish) {
+  return <Food name={dish.name} picture={dish.image} />;
+} */
 
 function App() {
   return (
@@ -56,8 +65,11 @@ function App() {
       />*/}
       {/*prop(property)를 fav를 만들고 kimchi라고 넣어줬다 */}
 
+      {/* {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)} */}
+
       {foodILike.map((dish) => (
-        <Food fav={dish.name} picture={dish.image} />
+        <Food key={dish.id} fav={dish.name} picture={dish.image} />
       ))}
       {/*{foodILike.map(function (dish) {
         return <Food fav={dish.name} />;
