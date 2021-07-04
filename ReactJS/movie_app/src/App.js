2262ad05ad88607338599a69650f7eda6ca632b0
ryula <hobby519@naver.com>
2021-07-04 13:37:09 +0900

    jsx part 1

--- ReactJS/movie_app/src/App.js
+++ ReactJS/movie_app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+//리액트가 빠른 이유: virtual DOM, virtual이라서 존재하지가 않음. 소스코드에서는 빈 index.html을 가져오고 그 다음에 html을 root있는 곳에 넣는다.
+//component: html을 반환하는 함수
+//react는 component를 사용해서 html처럼 작성하려는 경우에 필요하다. javascript와 html사이의 이러한 조합을 jsx라고 부른다.
+import Potato from "./potato";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!</h1>
+      <Potato />
     </div>
   );
 }
---
+++ ReactJS/movie_app/src/potato.js
@@ -0,0 +1,5 @@
+function Potato() {
+  return <h3>I like potato</h3>;
+}
+
+export default Potato;
