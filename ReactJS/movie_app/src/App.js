839f3f5ab0f4fa1e7c94b6c763065d05f00ce7c3
ryula <hobby519@naver.com>
2021-07-05 01:18:54 +0900

    map+object

--- ReactJS/movie_app/src/App.js
+++ ReactJS/movie_app/src/App.js
@@ -7,24 +7,65 @@
   console.log(props.fav);
   return <h3>I like potato</h3>;
 }*/
-function Food({ fav }) {
+function Food({ fav, picture }) {
   //object 내부의 property를 받는 방법
-  return <h3>I like {fav}</h3>;
+  return (
+    <div>
+      <h3>I like {fav}</h3>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Food
+      {/*<Food
         fav="kimchi"
         something={true}
         papapapa={["hello", 1, 3, 1.22, true]}
-      />
+      />*/}
       {/*prop(property)를 fav를 만들고 kimchi라고 넣어줬다 */}
-      <Food fav="ramen" />
+
+      {foodILike.map((dish) => (
+        <Food fav={dish.name} picture={dish.image} />
+      ))}
+      {/*{foodILike.map(function (dish) {
+        return <Food fav={dish.name} />;
+      })}과 같다*/}
+
+      {/*<Food fav="ramen" />
       <Food fav="kimbab" />
-      <Food fav="ansim" />
+    <Food fav="ansim" />*/}
     </div>
   );
 }
