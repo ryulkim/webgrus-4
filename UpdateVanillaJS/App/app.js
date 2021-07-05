e63eca0964554490a0ab6273fb89929efc97415a
ryula <hobby519@naver.com>
2021-07-06 02:18:44 +0900

    getting username

--- UpdateVanillaJS/App/app.js
+++ UpdateVanillaJS/App/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault(); //기본 동작을 하지 못하도록 함=>페이지 새로고침을 막음
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  console.log(username);
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 loginForm.addEventListener("submit", onLoginSubmit);
