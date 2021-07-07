4cc7620d4720690575cb04a7d7a7ea7da342fa4a
ryula <hobby519@naver.com>
2021-07-07 15:38:22 +0900

    clock

--- UpdateVanillaJS/App/js/clock.js
+++ UpdateVanillaJS/App/js/clock.js
@@ -1,7 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setInterval(sayHello, 1000); //5000ms=5s
+//setInterval(sayHello, 1000); //5000ms=5s, 해당 시간 마다 실행
+//setTimeout(sayHello, 1000); //해당 시간만큼 기다리고 실행
+getClock();
+setInterval(getClock, 1000);
