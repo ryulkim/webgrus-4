27f6e262d5b75e7c3061e5640398aa9ef7317ef7
ryula <hobby519@naver.com>
2021-07-07 21:04:25 +0900

    Life Cycle

--- ReactJS/movie_app/src/App.js
+++ ReactJS/movie_app/src/App.js
@@ -1,17 +1,33 @@
 import React from "react";
-import propTypes from "prop-types";
 
+//state를 사용하기 위해 class component 사용
+//Mount: constructor가 실행되고 그 담에 render가 실행된다. constructor->render->componentDidMount
+//Update: componentDidUpdate: render가 더 실행되면 (업데이트 되면) 발동!
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0,
   };
   add = () => {
-    console.log("add");
+    this.setState(current=>({count:current.count+1}));
   };
   minus = () => {
-    console.log("minus");
+    this.setState(current=>({count:current.count-1}));
   };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just updated!");
+  }
+  componentWillUnmount(){
+    console.log("Good Bye, cruel world!")
+  }
   render() {
+    console.log("I'm rendering.");
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
