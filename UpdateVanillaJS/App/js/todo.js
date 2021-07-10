dfc2dc4ae10fad1b22ed110370738b775efe087b
ryula <hobby519@naver.com>
2021-07-10 15:07:02 +0900

    foreach&arrow function

--- UpdateVanillaJS/App/js/todo.js
+++ UpdateVanillaJS/App/js/todo.js
@@ -1,13 +1,16 @@
 //JSON.stringify: 모든걸 string으로 바꿔줌
+//JSON.parse: 살아있는 object를 만들 수 있음
 
 const toDoForm=document.getElementById("todo-form");
 const toDoInput=toDoForm.querySelector("input");
 const toDoList=document.getElementById("todo-list");
 
+const TODOS_KEY="todos";
+
 const toDos=[];
 
 function saveToDos(){
-    localStorage.setItem("todos",JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
 }
 
 function deleteTodo(event){
@@ -37,3 +40,11 @@ function handleToDoSubmit(event){
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const savedToDos=localStorage.getItem(TODOS_KEY);
+
+console.log(savedToDos);
+if(savedToDos!==null){
+    const parsedToDos=JSON.parse(savedToDos);
+    parsedToDos.forEach((item)=>console.log(item));//arrow function(화살표 함수)
+}
