35359546b3e17cfd7c0f91c37d097990d1b40578
ryula <hobby519@naver.com>
2021-07-10 14:51:49 +0900

    save todo

--- UpdateVanillaJS/App/js/todo.js
+++ UpdateVanillaJS/App/js/todo.js
@@ -1,6 +1,14 @@
+//JSON.stringify: 모든걸 string으로 바꿔줌
+
 const toDoForm=document.getElementById("todo-form");
 const toDoInput=toDoForm.querySelector("input");
 const toDoList=document.getElementById("todo-list");
+
+const toDos=[];
+
+function saveToDos(){
+    localStorage.setItem("todos",JSON.stringify(toDos));
+}
 
 function deleteTodo(event){
     const li=event.target.parentElement;
@@ -23,7 +31,9 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo=toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
