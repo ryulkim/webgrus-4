421cb0af5f7ae074b0a178367b5fe0df29eae5e4
ryula <hobby519@naver.com>
2021-07-12 01:00:54 +0900

    weather

--- UpdateVanillaJS/App/js/weather.js
+++ UpdateVanillaJS/App/js/weather.js
@@ -1,10 +1,21 @@
-function onGeoOk(position){
-    const lat=position.coords.latitude;
-    const lng=position.coords.longitude;
-    console.log(lat, lng);
+const API_KEY = "6014809f85cd2245c0ebbc9f78846be6";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log(lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = data.name;
+    });
 }
-function onGeoError(){
-    alert("Can't find you. No weather for you.");
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
