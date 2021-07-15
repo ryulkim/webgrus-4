555cec7d183d48b83733fa8ebaba79fb2415c4c1
ryula <hobby519@naver.com>
2021-07-16 02:22:54 +0900

    cutting summary

--- ReactJS/movie_app/src/Movie.js
+++ ReactJS/movie_app/src/Movie.js
@@ -17,7 +17,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
               </li>
             ))}
           </ul>
-          <p className="movie__summary">{summary}</p>
+          <p className="movie__summary">{summary.slice(0, 140)}...</p>
         </div>
       </div>
     </div>
