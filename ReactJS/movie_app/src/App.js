fb51a3f32284fe292d42f493a6d5114f8266220c
ryula <hobby519@naver.com>
2021-07-18 13:43:36 +0900

    navigation

--- ReactJS/movie_app/src/App.js
+++ ReactJS/movie_app/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
