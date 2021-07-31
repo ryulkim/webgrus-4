a80456d4c65a23f73796a59560f8c422c04fbef7
ryula <hobby519@naver.com>
2021-07-31 15:09:48 +0900

    react to-do list

--- ReactJS/react0/src/App.js
+++ ReactJS/react0/src/App.js
@@ -5,15 +5,39 @@ import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 1: "sjkdjfkdjfkdjfkdjfdkjfdkfj", 2: "sdfsfsdfsdf" };
+    this.state = { list: [] };
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.firstElementChild.value);
+    let id = Math.random() * 5;
+
+    const newItem = {
+      id: id,
+      value: e.target.firstElementChild.value,
+    };
+
+    console.log(newItem);
+
+    this.setState({ list: this.state.list.concat(newItem) });
+    e.target.firstElementChild.value = "";
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div>
         <h1>Hello!</h1>
-        {this.state[1]}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="put to-do thing" />
+          <button type="submit">등록</button>
+        </form>
+
         <br />
-        {this.state[2]}
+        {this.state.list.map((item) => {
+          return <li key={item.id}>{item.value}</li>;
+        })}
       </div>
     );
   }
