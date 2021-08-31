15b66dfc5534bd3a134bd212255847aafb25ca20
ryula <hobby519@naver.com>
2021-08-31 16:51:48 +0900

    accordion

---
+++ ReactJS/accordion/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data.js";
+import SingleQuestion from "./Question.js";
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
