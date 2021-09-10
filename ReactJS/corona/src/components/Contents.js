58c9e507ab349499785aefcd6d16054a37e8bd68
ryula <hobby519@naver.com>
2021-09-11 04:25:20 +0900

    집계 사이트

--- ReactJS/corona/src/components/Contents.js
+++ ReactJS/corona/src/components/Contents.js
@@ -3,25 +3,58 @@ import { Bar, Doughnut, Line } from "react-chartjs-2";
 import axios from "axios";
 
 const Contents = () => {
-  const [confirmedData, setConfirmedData] = useState({
-    labels: ["1월", "2월", "3월"],
-    datasets: [
-      {
-        label: "국내 누적 확진자",
-        backgroundColor: "salmon",
-        fill: true,
-        data: [10, 5, 3],
-      },
-    ],
-  });
+  const [confirmedData, setConfirmedData] = useState({});
 
   useEffect(() => {
     const fetchEvents = async () => {
       const res = await axios.get(
         "https://api.covid19api.com/dayone/country/kr"
       ); //async, await를 통해 모든 데이터가 불러와질때까지 기다린다.
-      console.log(res);
+      makeData(res.data);
     };
+    const makeData = (items) => {
+      const arr = items.reduce((acc, cur) => {
+        const currentDate = new Date(cur.Date);
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const date = currentDate.getDate();
+        const confirmed = cur.Confirmed;
+        const active = cur.Active;
+        const death = cur.Deaths;
+        const recovered = cur.Recovered;
+
+        const findItem = acc.find((a) => a.year === year && a.month === month);
+
+        if (!findItem) {
+          acc.push({ year, month, date, confirmed, active, death, recovered });
+        }
+        if (findItem && findItem.date < date) {
+          findItem.active = active;
+          findItem.death = death;
+          findItem.date = date;
+          findItem.year = year;
+          findItem.month = month;
+          findItem.recovered = recovered;
+          findItem.confirmed = confirmed;
+        }
+        return acc;
+      }, []);
+      console.log(arr);
+      const labels = arr.map((a) => `${a.month + 1}월`);
+      console.log(labels);
+      setConfirmedData({
+        labels,
+        datasets: [
+          {
+            label: "국내 누적 확진자",
+            backgroundColor: "salmon",
+            fill: true,
+            data: arr.map((a) => a.confirmed),
+          },
+        ],
+      });
+    };
+
     fetchEvents();
   });
   return (
