e2723fc9c814947391e9a620998b6044f21e384d
ryula <hobby519@naver.com>
2021-09-12 15:30:42 +0900

    그래프 3개

--- ReactJS/corona/src/components/Contents.js
+++ ReactJS/corona/src/components/Contents.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 const Contents = () => {
   const [confirmedData, setConfirmedData] = useState({});
+  const [quarantinedData, setQuarantinedData] = useState({});
+  const [comparedData, setComparedData] = useState({});
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -53,10 +55,35 @@ const Contents = () => {
           },
         ],
       });
+      setQuarantinedData({
+        labels,
+        datasets: [
+          {
+            label: "월별 격리자 현황",
+            borderColor: "green",
+            backgroundColor: "green",
+            fill: false,
+            data: arr.map((a) => a.active),
+          },
+        ],
+      });
+      const last = arr[arr.length - 1];
+      setComparedData({
+        labels: ["확진자", "격리해제", "사망"],
+        datasets: [
+          {
+            label: "누적 확진, 해제, 사망 비율",
+            backgroundColor: ["#ff3d67", "#059bff", "#ffc233"],
+            borderColor: ["#ff3d67", "#059bff", "#ffc233"],
+            fill: false,
+            data: [last.confirmed, last.recovered, last.death],
+          },
+        ],
+      });
     };
 
     fetchEvents();
-  });
+  }, []);
   return (
     <section>
       <h2>국내 코로나 현황</h2>
@@ -69,6 +96,34 @@ const Contents = () => {
                 title: {
                   display: true,
                   text: "누적 확진자 추이",
+                  fontSize: 16,
+                },
+              },
+              { legend: { display: true, position: "bootom" } })
+            }
+          />
+          <Line
+            data={quarantinedData}
+            options={
+              ({
+                title: {
+                  display: true,
+                  text: "월별 격리자 현황",
+                  fontSize: 16,
+                },
+              },
+              { legend: { display: true, position: "bootom" } })
+            }
+          />
+          <Doughnut
+            data={comparedData}
+            options={
+              ({
+                title: {
+                  display: true,
+                  text: `누적 확진, 해제, 사망 (${
+                    new Date().getMonth() + 1
+                  }월 데이터)`,
                   fontSize: 16,
                 },
               },
