14714c217b5790011541a5986fe71e951ad16d80
Ryula <hobby519@naver.com>
2021-11-14 04:44:16 +0900

    Fragment

--- ReactJS/gugudan/src/App.js
+++ ReactJS/gugudan/src/App.js
@@ -32,14 +32,14 @@ class GuGuDan extends React.Component{
 
   render(){
     return(
-      <div>
+      <React.Fragment>
         <div>{this.state.first}곱하기{this.state.second}는?</div>
         <form onSubmit={this.onSubmit}>
           <input type="number" value={this.state.value} onChange={(e)=>this.setState({value:e.target.value})}/>
           <button>입력!</button>
         </form>
         <div>{this.state.result}</div>
-      </div>
+      </React.Fragment>
     );
   }
 }
