be69b8be1c932f834cca8901ea9a63f2e24b6b68
Ryula <hobby519@naver.com>
2021-11-14 05:00:53 +0900

    ref

--- ReactJS/gugudan/src/App.js
+++ ReactJS/gugudan/src/App.js
@@ -15,27 +15,33 @@ class GuGuDan extends React.Component{
   onSubmit=(e)=>{
     e.preventDefault();
     if(parseInt(this.state.value)===this.state.first*this.state.second){
-      const value=this.state.value;
-      this.setState({
-        result: value+' 정답',
-        first: Math.ceil(Math.random()*9),
-        second: Math.ceil(Math.random()*9),
-        value: '',
+      this.setState((prevState)=>{
+        return{
+          result: prevState.value+' 정답',
+          first: Math.ceil(Math.random()*9),
+          second: Math.ceil(Math.random()*9),
+          value: '',
+        };
       });
+      this.input.focus();
     }else{
       this.setState({
         result: '땡',
         value: '',
       });
+      this.input.focus();
     }
+  }
+  onRefInput=(c)=>{
+    this.input=c;
   }
 
   render(){
     return(
       <React.Fragment>
-        <div>{this.state.first}곱하기{this.state.second}는?</div>
+        <div>{this.state.first} 곱하기 {this.state.second}는?</div>
         <form onSubmit={this.onSubmit}>
-          <input type="number" value={this.state.value} onChange={(e)=>this.setState({value:e.target.value})}/>
+          <input ref={this.onRefInput} type="number" value={this.state.value} onChange={(e)=>this.setState({value:e.target.value})}/>
           <button>입력!</button>
         </form>
         <div>{this.state.result}</div>
